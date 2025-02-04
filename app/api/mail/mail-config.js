6cfa2fc421d12883555863d7e9d0a9019e1af810
Tamjid Ahmed <tam74353@gmail.com>
2025-02-04 18:50:38 +0600

    change mail port

--- app/api/mail/mail-config.js
+++ app/api/mail/mail-config.js
@@ -11,7 +11,7 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
+  port: 465,
   secure: true, // Use false if your server doesn't use SSL/TLS
   auth: {
     user: process.env.GMAIL,
